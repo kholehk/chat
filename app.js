b72a49bf1e5ae29d0b28f35d13c4e5f03d30657b
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2021-03-20 13:53:30 +0200

    correct route '/ws' on server side

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.get('/ws', (req) => {
+app.get('/ws', (req, res) => {
   if (
     !req.headers.upgrade
     || req.headers.upgrade.toLowerCase() !== 'websocket'
     || !req.headers.connection.match(/\bupgrade\b/i)
-  ) return;
+  ) {
+    res.redirect('/');
+  }
 
   wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
 });
